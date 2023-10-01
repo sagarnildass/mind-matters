719e1cb32a047089878a4237d6e3ece7150af13b
senthil <svelan@artelus.com>
2023-10-01 10:54:53 +0530

    Mobile responsive fix

--- frontend/src/components/NavbarHeader.jsx
+++ frontend/src/components/NavbarHeader.jsx
@@ -11,12 +11,13 @@ import Navbar from './Navbar';
 const NavbarHeader = (props) => {
 
     return (
-               <header className=" bg-nav  w-100 ">
+                <>
+               <header className="shrink-0 bg-nav  w-100 ">
                 <div className="flex justify-between ">
                     <div className="p-1 mx-3 inline-flex items-center">
                        <FontAwesomeIcon onClick={props.sidebarToggle}  icon={faBars} className="h-6 w-6 2xl:h-8 2xl:w-8 ml-2 text-blue-200  transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white dark:text-gray-400" />
                         <h1 className="text-white p-2">
-                        <Link to="/" className="flex items-center pl-2.5 mb-2">
+                        <Link to="/" className="">
                            {/*  <div className={styles.logo} className={styles.visionUiPro}>
                             */}
                                 <div   className= "text-gradient-white  mt-2" >
@@ -35,6 +36,7 @@ const NavbarHeader = (props) => {
                     </div>
                 </div>
             </header>
+            </>
     );
 };
 
